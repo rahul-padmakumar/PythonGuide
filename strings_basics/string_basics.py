a2e1c855ea1b0df800a36386a79118ee7874fc28
rahul <rahulp5875@gmail.com>
2024-06-24 21:50:56 +0530

    Python string operations

---
+++ strings_basics/string_basics.py
@@ -0,0 +1,24 @@
+print("Hi all")
+
+print("Hi \" all \"")
+
+print("Hi \nHello")
+
+print('hello tom')
+
+print("hello tom''")
+
+print("""
+Label -> Value
+one   -> 1
+two   -> 2
+""")
+
+print(r'C:\some\name')
+
+print('py' 'thon') # only works with string literals not the variables
+
+value1 = "hello"
+value2 = " Tester"
+print(value1 + value2)
+
---
+++ strings_basics/string_indexing.py
@@ -0,0 +1,24 @@
+name = "Tester"
+
+print(name[0])
+print(name[-1])
+
+print(name[-1] == name[len(name) - 1])
+
+print(name[:])
+
+print(name[2:])
+
+print(name[2:4])
+
+print(name[:4])
+
+print(name[::2])
+
+print(name[1:5:2])
+
+print(name[1:] + name[:1])
+
+print(name[::-1])
+
+print("malayalam" == "malayalam"[::-1])
