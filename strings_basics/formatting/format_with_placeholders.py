106e200d9f4c9e44cd930f6217c27bd08def6b9d
rahul <rahulp5875@gmail.com>
2024-06-25 16:26:45 +0530

    Update python formatting with placeholders.

--- strings_basics/formatting/format_with_placeholders.py
+++ strings_basics/formatting/format_with_placeholders.py
@@ -5,4 +5,13 @@ print("%s %s!" % ('Python', 'rocks'))
 x, y = 'Python', 'rocks'
 print("%s %s!" % (x, y))
 
+print("Python %s" % "\trocks")
+print("Python %r" % "\trocks")
 
+print("%s" % 11.256)
+print("%d" % 11.256)
+print("%f" % 11.256)
+
+print("%3.5f" % 11.25678934)
+
+print("%s is %3.2f %r" % ("Number", 13.2456789, "Yay!!!"))
