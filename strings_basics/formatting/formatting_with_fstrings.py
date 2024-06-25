e98f13152f08bd8000785883ae1b0427e047e755
rahul <rahulp5875@gmail.com>
2024-06-25 16:33:11 +0530

    Update python formatting with fstrings.

---
+++ strings_basics/formatting/formatting_with_fstrings.py
@@ -0,0 +1,9 @@
+name = "Tester"
+print(f"Hello {name}")
+
+number = 10.34562
+print(f"The input is {number:{1}.{4}}") # precision refers to total number of digits when used in this format
+
+print(f"The input is {number:1.2f}")
+
+
