9727bd35476e753f5ba9eb760e90c6c78ab1d048
rahul <rahulp5875@gmail.com>
2024-07-04 12:57:24 +0530

    Python set examples

---
+++ data_structures/SetsBasics.py
@@ -0,0 +1,15 @@
+set1 = set()
+print(set1)
+print(type(set1))
+
+set1.add(1)
+print(set1)
+
+set1.add(2)
+print(set1)
+
+set1.add(1)
+print(set1)
+
+list1 = [1, 2, 2, 4, 4, 5, 5, 5, 3, 3]
+print(set(list1))
