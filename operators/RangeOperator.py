c15bfee6d88f67762cd55bd5e3a088e1ace0b5d4
rahul <rahulp5875@gmail.com>
2024-09-06 15:38:20 +0530

    Add example for range operator.

---
+++ operators/RangeOperator.py
@@ -0,0 +1,16 @@
+
+for i in range(10):
+    print(i)
+
+print("\n")
+
+for i in range(10, 20, 2):
+    print(i)
+
+print("\n")
+
+print(list(range(10)))
+
+print("\n")
+
+print(list(range(10, 50, 5)))
