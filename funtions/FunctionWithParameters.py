8c046ba94c973aa4171b0d0863e6e37542b72807
rahul <rahulp5875@gmail.com>
2024-09-09 11:39:28 +0530

    Add example for functions basics.

---
+++ funtions/FunctionWithParameters.py
@@ -0,0 +1,6 @@
+
+def print_message(message):
+    print("Hello {}".format(message))
+
+
+print_message("Test dev")
---
+++ funtions/FunctionsWithReturn.py
@@ -0,0 +1,6 @@
+
+def sum_of_numbers(num1, num2):
+    return num1 + num2
+
+
+print(sum_of_numbers(4, 5))
