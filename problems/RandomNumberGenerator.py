c1a942c205175c09314bf94ec78a558ca52565bf
rahul <rahulp5875@gmail.com>
2024-09-20 14:50:14 +0530

    Minor code change.

--- problems/RandomNumberGenerator.py
+++ problems/RandomNumberGenerator.py
@@ -2,9 +2,10 @@ import random
 
 
 def rand_num(low, high, limit):
-    for num in range(limit):
+    for i in range(limit):
         yield random.randint(low, high)
 
 
 for num in rand_num(10, 100, 10):
     print(num)
+
