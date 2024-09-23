ff113ade3c0b845d7a4f0091b2ecb7df4c4d8016
rahul <rahulp5875@gmail.com>
2024-09-23 13:07:21 +0530

    Add examples of math module.

---
+++ random_modules/MathModule.py
@@ -0,0 +1,21 @@
+import math
+
+print(math.pi)
+
+v1 = 4.456
+print(math.ceil(v1))
+print(math.floor(v1))
+print(round(v1))
+print(round(5.5))
+
+print(math.e)
+
+print(math.log(math.e))
+
+print(math.log(100, 10))
+
+print(math.cos(30))
+
+print(math.degrees(math.pi / 2))
+
+print(math.radians(180))
