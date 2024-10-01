937bad3860b1f4a4631da61e847da5b035f241b8
rahul <rahulp5875@gmail.com>
2024-10-01 14:09:40 +0530

    Add example for random number functons.

---
+++ random_modules/RandomNumberFunctions.py
@@ -0,0 +1,27 @@
+
+# Hex representation
+
+print(hex(13))
+print(hex(15))
+print(hex(30))
+
+# Binary Rep
+
+print(bin(123))
+print(bin(7))
+
+# Pow
+print(pow(2, 4))
+print(pow(2, 4, 3))  # equivalent to 2^4%3
+
+# abs
+
+print(abs(-59.34))
+print(abs(30))
+print(abs(-30))
+
+
+
+
+
+
